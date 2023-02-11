b820818f9788f888fbd2eac28ac4af96320e95a1
IVKazantsev <97786849+IVKazantsev@users.noreply.github.com>
2023-02-11 11:57:35 +0200

    Add files via upload

---
+++ Functions/SomeFunctions.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <math.h>
+
+const int N_max = 1000;
+
+bool isPrime(int x)
+
+
+// Обмен двух переменных
+// void - функция ничего не возвращает (процедура)
+void swap(int& a, int& b) // & - это передача по ссылке
+
+
+// Сортировка
+void sort(int n, int mas[N_max])
+
+
+// Поиск суммы цифр числа
+int sumOfDigits(int x)
